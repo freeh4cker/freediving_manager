d1b23bc914b229796c629ac01edde588998300f5
Bertrand Néron <freeh4cker@gmail.com>
2024-11-26 22:42:35 +0100

    add license motice in source code

--- fdm/contest.py
+++ fdm/contest.py
@@ -1,3 +1,24 @@
+#############################################################################
+# Freediving Manager is a program to help to manage                         #
+#            A Depth freediving Contest to manage start lists, results, ... #
+# Authors: Bertrand Néron                                                   #
+# Copyright (C) 2024  Bertrand Néron                                       #
+#                                                                           #
+# This program is free software: you can redistribute it and/or modify      #
+# it under the terms of the GNU General Public License as published by      #
+# the Free Software Foundation, either version 3 of the License, or         #
+# (at your option) any later version.                                       #
+#                                                                           #
+# This program is distributed in the hope that it will be useful,           #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                      #
+# See the GNU General Public License for more details.                      #
+#                                                                           #
+# You should have received a copy of the GNU General Public License         #
+# along with this program (COPYING).                                        #
+# If not, see <https://www.gnu.org/licenses/>.                              #
+#############################################################################
+
 from __future__ import  annotations
 import datetime
 
@@ -8,6 +29,13 @@ from .dive import Dive
 class Contest:
 
     def __init__(self, name: str, date:datetime.date, warmup: int = 45, lines: int = 1) -> None:
+        """
+
+        :param name:
+        :param date:
+        :param warmup:
+        :param lines:
+        """
         self.name = name
         self.date = date
         self.warmup = warmup
--- fdm/dive.py
+++ fdm/dive.py
@@ -1,3 +1,24 @@
+#############################################################################
+# Freediving Manager is a program to help to manage                         #
+#            A Depth freediving Contest to manage start lists, results, ... #
+# Authors: Bertrand Néron                                                   #
+# Copyright (C) 2024  Bertrand Néron                                       #
+#                                                                           #
+# This program is free software: you can redistribute it and/or modify      #
+# it under the terms of the GNU General Public License as published by      #
+# the Free Software Foundation, either version 3 of the License, or         #
+# (at your option) any later version.                                       #
+#                                                                           #
+# This program is distributed in the hope that it will be useful,           #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                      #
+# See the GNU General Public License for more details.                      #
+#                                                                           #
+# You should have received a copy of the GNU General Public License         #
+# along with this program (COPYING).                                        #
+# If not, see <https://www.gnu.org/licenses/>.                              #
+#############################################################################
+
 from .rules import Remark, Card, Discipline
 
 
@@ -33,7 +54,7 @@ class Performance:
 
     def compute_points(self) -> int:
         """
-        
+
         :return:
         """
         points = self.depth
@@ -60,5 +81,3 @@ class Dive:
         self.performance = None
         self.official_time = None
         self.lane = None
-
-
